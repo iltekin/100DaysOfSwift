1b49dc6bc987df6f0ec30c8ae945dfac74b34ce4
iltekin <sezer@iltekin.com>
2022-01-05 01:03:12 +0300

    Completed DetailView

---
+++ Day 023/Country Flags/Country Flags/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Country Flags
+//
+//  Created by Sezer İltekin on 5.01.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var myLabel: UILabel!
+    var flag: String?
+    var country: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let flag = flag {
+            imageView.image = UIImage(named: flag)
+        }
+        
+        if let country = country {
+            myLabel.text = country
+        }
+        
+    }
+    
+}
--- Day 023/Country Flags/Country Flags/ViewController.swift
+++ Day 023/Country Flags/Country Flags/ViewController.swift
@@ -9,24 +9,53 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var flags = [String]()
+    var countries = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Country Flags"
+        navigationController?.navigationBar.prefersLargeTitles = false
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
 
-        print(items)
+        for item in items {
+            if item.hasSuffix(".png") {
+                let countryName = item.components(separatedBy: ".")[0].uppercased()
+                countries.append(countryName)
+                flags.append(item)
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return countries.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Cell \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            print("Deleted \(countries[indexPath.row]) Flag")
+            flags.remove(at: indexPath.row)
+            countries.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "detailVC") as? DetailViewController {
+            vc.flag = flags[indexPath.row]
+            vc.country = countries[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
