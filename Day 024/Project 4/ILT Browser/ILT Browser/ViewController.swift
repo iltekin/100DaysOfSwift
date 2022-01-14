dfa071e2fc0f49130bc90bc0226cf4e2652ec53d
iltekin <sezer@iltekin.com>
2022-01-15 00:50:43 +0300

    set up some web view

--- Day 024/Project 4/ILT Browser/ILT Browser/ViewController.swift
+++ Day 024/Project 4/ILT Browser/ILT Browser/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://iltekin.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
 
 
