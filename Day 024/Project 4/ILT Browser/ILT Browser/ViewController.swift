a619cd99cdafacf2b396ebf922483cda3ccd3a59
iltekin <sezer@iltekin.com>
2022-01-14 23:54:34 +0300

    new project added

---
+++ Day 024/Project 4/ILT Browser/ILT Browser/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ILT Browser
+//
+//  Created by Sezer İltekin on 14.01.2022.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
