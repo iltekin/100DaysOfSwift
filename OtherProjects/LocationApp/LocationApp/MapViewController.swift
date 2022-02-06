023eeff206780edc57134a76036d9878b855e6a2
iltekin <sezer@iltekin.com>
2022-02-06 23:57:12 +0300

    created connection between views and completed tableview

--- OtherProjects/LocationApp/LocationApp/MapViewController.swift
+++ OtherProjects/LocationApp/LocationApp/MapViewController.swift
@@ -8,25 +8,31 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
     
+    var selectedLatitude = Double()
+    var selectedLongitude = Double()
+    
+    var receivedName = ""
+    var receivedId: UUID?
+
     var locationManager = CLLocationManager()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Add Place"
+        saveButton.isEnabled = false
         
         mapView.delegate = self
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
         
         let dismissKeyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(dismissKeyboardRecognizer)
@@ -34,6 +40,15 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(selectLocation(gestureRecognizer:)))
         gestureRecognizer.minimumPressDuration = 1
         mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if receivedName != "" {
+            if let id = receivedId?.uuidString {
+                fillData(id: id)
+            }
+        } else {
+            locationManager.startUpdatingLocation()
+        }
+        
     }
     
     @objc func dismissKeyboard(){
@@ -43,14 +58,24 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     @objc func selectLocation(gestureRecognizer: UILongPressGestureRecognizer){
         
         if gestureRecognizer.state == .began {
+            
+            let annotations = mapView.annotations
+            if !annotations.isEmpty{
+                mapView.removeAnnotations(annotations)
+            }
+            
             let touchPoint = gestureRecognizer.location(in: mapView)
             let touchToCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
             
+            selectedLatitude = touchToCoordinate.latitude
+            selectedLongitude = touchToCoordinate.longitude
+
             let annotation = MKPointAnnotation()
             annotation.coordinate = touchToCoordinate
             annotation.title = nameTextField.text
             annotation.subtitle = descriptionTextField.text
             mapView.addAnnotation(annotation)
+            saveButton.isEnabled = true
         }
         
     }
@@ -68,6 +93,89 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     
     @IBAction func saveLocation(_ sender: Any) {
+        
+        if nameTextField.text == "" {
+            showAlert(title: "One more step", message: "You must provide a name for place", buttonText: "OK")
+            redBorder(textfield: nameTextField)
+            return
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlace.setValue(nameTextField.text, forKey: "name")
+        newPlace.setValue(descriptionTextField.text, forKey: "desc")
+        newPlace.setValue(selectedLatitude, forKey: "latitude")
+        newPlace.setValue(selectedLongitude, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "newPlace"), object: nil)
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Error!")
+        }
+
+    }
+    
+    func showAlert(title: String, message: String, buttonText: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertButton = UIAlertAction(title: buttonText, style: .default, handler: nil)
+        alertController.addAction(alertButton)
+        present(alertController, animated: true)
+    }
+    
+    func redBorder(textfield: UITextField){
+        textfield.layer.cornerRadius = 4
+        textfield.layer.borderWidth = 1
+        textfield.layer.borderColor = UIColor.red.cgColor
+    }
+    
+    func fillData(id: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        fetchRequest.predicate = NSPredicate(format: "id = %@", id)
+        fetchRequest.returnsObjectsAsFaults = false
+
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    
+                    if let name = result.value(forKey: "name") as? String {
+                        if let desc = result.value(forKey: "desc") as? String {
+                            if let latitude = result.value(forKey: "latitude") as? Double {
+                                if let longitude = result.value(forKey: "longitude") as? Double {
+                                    nameTextField.text = name
+                                    descriptionTextField.text = desc
+                                    
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    let region = MKCoordinateRegion(center: coordinate, span: span )
+                                    mapView.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                    annotation.coordinate = coordinate
+                                    annotation.title = name
+                                    annotation.subtitle = desc
+                                    mapView.addAnnotation(annotation)
+                                    
+                                    saveButton.isEnabled = false
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        } catch {
+            print("Error!")
+        }
         
     }
     
--- OtherProjects/LocationApp/LocationApp/ViewController.swift
+++ OtherProjects/LocationApp/LocationApp/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var selectedName = ""
+    var selectedId: UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,21 +25,77 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        fetchData()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let selected = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: selected, animated: true)
+        }
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchData), name: NSNotification.Name("newPlace"), object: nil)
+    }
+    
+    @objc func fetchData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                
+                nameArray.removeAll(keepingCapacity: false)
+                idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                }
+                
+                tableView.reloadData()
+                
+            }
+        } catch {
+            print("Error!")
+        }
+        
     }
     
     @objc func addPlace() {
+        selectedName = ""
         performSegue(withIdentifier: "toMapVC", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test" + String(indexPath.row)
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedName = nameArray[indexPath.row]
+        selectedId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toMapVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toMapVC" {
+            let destination = segue.destination as! MapViewController
+            destination.receivedName = selectedName
+            destination.receivedId = selectedId
+        }
+    }
 
 }
